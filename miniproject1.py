e6901d6ac723179a96199f57b3e17a5df9f21774
chaitanyareddy4 <ustalamurichaitanya@gmail.com>
2026-07-23 16:58:55 +0530

    used f strings

--- miniproject1.py
+++ miniproject1.py
@@ -8,7 +8,8 @@ cgpa=float(input("enter CGPA:"))
 #display the student details
 
 print("Student Details:")
-print("Name:", name)
-print("Branch:", branch)
-print("Year:", year)
-print("CGPA:", cgpa)
+
+print(f"Name: {name}")
+print(f"Branch: {branch}")
+print(f"Year: {year}")
+print(f"CGPA: {cgpa}")
